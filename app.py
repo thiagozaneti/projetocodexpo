20964b7cb45eff299f6155f311b47e73709d7d6e
ThiagoZaneti <th2006zadosa@gmail.com>
2023-12-04 01:32:34 -0300

    algoritmo de caracter feito forms com verificacao feito falta paterns nas textbox

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_login import LoginManager, login_user, current_user, UserMixin, login_required, logout_user
 from flask_mail import Mail
+import random
+import string
 
 
 app = Flask(__name__)
@@ -67,6 +69,9 @@ class Buy(db.Model, UserMixin):
     name_of_propriety = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(200), nullable=False)
     selecao = db.Column(db.String(200), nullable=False)
+    sequencia = db.Column(db.String(8), nullable=False)
+
+
 
 
 
@@ -93,14 +98,14 @@ def register():
         username = request.form.get('nome')
         email = request.form.get('email')
         password = request.form.get('senha')
-        if len(password) <= 4:
-            message = "Senha fraca"
-        elif len(email) <= 5:
-            message = "E-mail fraco"
-        elif len(username) <= 4:
-            message = "Nome de usuário fraco"
-        elif len(username) > 20:
-            message = "Usuário excedente, mínimo 20 caracteres"
+        if len(username) < 3:
+            return redirect('register')
+        elif len(username) > 30:
+            return redirect('register')
+        elif len(password) < 6:
+            return redirect('register')
+        elif len(password) > 16:
+            return redirect('register')
         else:
             new_user = User(username=username, email=email, password=password)
             db.session.add(new_user)
@@ -118,16 +123,20 @@ def login():
         password = request.form.get('senha')
         user = User.query.filter_by(email=email, password=password).first()
         admin_user = Admin.query.filter_by(email=email, password=password).first()  # Rename admin to admin_user
-        
-        if user:
+        if len(password) < 6:
+            message = 'senha com poucos caracteres'
+        elif len(password) > 16:
+            message = 'senha ultrapassando caracteres'
+        elif user:
             login_user(user)
             return redirect(url_for('system'))
-        if admin_user:
+        elif admin_user:
             login_user(admin_user)
             return redirect(url_for('admin'))
+        elif admin_user == user:
+            return redirect('login')
         else:
             message = "Usuário não encontrado, tente novamente"
-            return redirect(url_for('index'))
     return render_template('login.html', message=message)
 
    
@@ -160,29 +169,45 @@ def systemShop():
 @app.route('/system_profile')
 @login_required
 def system_profile():
-    return render_template('system/systemUser.html')
+    username = current_user.username
+    email = current_user.email
+    nova_sequencia = session.pop('nova_sequencia', None)
+    return render_template('system/systemUser.html', nova_sequencia = nova_sequencia, username = username, email = email)
 
 @app.route('/system_Buy', methods = ['POST','GET'])
 @login_required
 def systemBuy():
+    message = ''
     if request.method == 'POST':
         number_card = request.form.get('number_card')
         date_expires = request.form.get('date_expires')
         cvv = request.form.get('cvv')
         name_of_propriety = request.form.get('name_of_propriety')
         email = request.form.get('email')
-
-
-        new_buy = Buy(number_card = number_card, date_expires = date_expires, cvv = cvv, name_of_propriety = name_of_propriety, email = email)
-        db.session.add(new_buy)
-        db.session.commit()
-    return render_template('system/systemBuy.html')
+        if len(str(number_card)) != 16:
+            message = 'Numero de cartao errado'
+        elif len(date_expires) !=4:
+            message = 'Numero de fim errado'
+        elif len(cvv) !=3:
+            message = 'Cvv errado'
+        elif len(str(number_card)) != 16 and len(date_expires) !=4 and len(cvv) !=3:
+            message = 'Todos os campos possuem poucos caracteres'
+        else:
+        ##algoritmo gerador de sequencia
+            nova_sequencia = ''.join(random.choices(string.ascii_uppercase+ string.digits, k=8))
+            session['nova_sequencia'] = nova_sequencia ## armazena variaveis em lista que recebem a variavel para a sessao do usuário
+            ##fim do algoritmo
+            new_buy = Buy(number_card = number_card, date_expires = date_expires, cvv = cvv, name_of_propriety = name_of_propriety, email = email, sequencia = nova_sequencia)
+            db.session.add(new_buy)
+            db.session.commit()
+            return redirect(url_for('systemBuy', nova_sequencia = nova_sequencia) )
+    return render_template('system/systemBuy.html', message = message)
 
 
 ##<---------------------------------->
 
 @app.route('/homeadm')
-@login_required
+
 def homeadm():
     username = current_user.username
     email = current_user.email
@@ -306,6 +331,5 @@ if __name__ == "__main__":
     login_manager.init_app(app)
 
 
-##//TODO:TERMINAR VERIFICAÇÃO DO LOGIN DE USUÁRIO 
 ##//TODO:Desenvolver sistema de email
-##//TODO:Desenvolver algoritmo random identificador 
+##//TODO:Desenvolver pagina de usuário com 
